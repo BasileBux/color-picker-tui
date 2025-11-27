46e2d967552726f01178cd58273b3a5305ef2817
BasileBux <basile.buxtorf@ik.me>
2025-11-27 13:50:18 +0100

    wayland clipboard is now persistent after app is closed

--- src/clipboard/copy.rs
+++ src/clipboard/copy.rs
@@ -1,8 +1,23 @@
 use arboard::Clipboard;
-use std::io::{self};
+use std::{
+    io::{self},
+    process::Command,
+};
 
+/// Warning no input validation is done on args inside of this
+/// function - be careful when using user input.
+pub fn wl_copy(arg: &str) -> io::Result<std::process::Child> {
+    Command::new("wl-copy").arg(arg).spawn()
+}
+
+// We have to use a special method to copy to clipboard on Wayland because arboard
+// will not write to wl-copy so when the prgram is closed, the clipboard is lost.
 pub fn clipboard_copy(str: &str) -> io::Result<()> {
-    let mut clipboard = Clipboard::new().unwrap();
-    clipboard.set_text(str).unwrap();
+    if std::env::var("WAYLAND_DISPLAY").is_ok() {
+        wl_copy(str)?;
+    } else {
+        let mut clipboard = Clipboard::new().unwrap();
+        clipboard.set_text(str).unwrap();
+    }
     Ok(())
 }
